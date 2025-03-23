cbafb4d57090904e564f00572cc699088ec9976e
alem633 <mannaalessio885@gmail.com>
2025-03-23 23:49:07 +0100

    fixes

--- 1337x.sh
+++ 1337x.sh
@@ -8,8 +8,9 @@ fi
 
 rm -rf paths.txt
 
+query=$(echo "$1" | tr ' ' '+')
 for ((i=1; i<=$2; i++)); do
-    curl -s "https://www.1337x.to/search/$1/$i/" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br, zstd' -H 'Referer: https://www.1337x.to/search/italiano/1/' -H 'Alt-Used: www.1337x.to' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Priority: u=0, i' -H 'TE: trailers' > search.html
+    curl -s "https://www.1337x.to/search/$query/$i/" --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br, zstd' -H 'Referer: https://www.1337x.to/search/italiano/1/' -H 'Alt-Used: www.1337x.to' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Priority: u=0, i' -H 'TE: trailers' > search.html
     python3 parser.py >> paths.txt
 done
 
